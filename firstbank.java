a5a849de843a865fa60e7f8277d481e4476ef99f
Ajayvenkats <85541626+Ajayvenkats@users.noreply.github.com>
2021-06-08 11:19:52 +0530

    Add files via upload

---
+++ firstbank.java
@@ -0,0 +1,11 @@
+package tarining;
+
+public class firstbank {
+    public static void main(String[] args){
+        account Aj =new account("Ajay","A002");
+
+        account as =new account("ajay","a2");
+        as.ShowMenu();
+    }
+}
+
